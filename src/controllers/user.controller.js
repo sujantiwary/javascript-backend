d655b9897f88f9995e6840de936801bd009a451e
sujantiwary <sujantiwary5657@gmail.com>
2026-01-19 12:23:50 +0545

    fix: correct typo in subscriber field and add getWatchHistory functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -361,7 +361,7 @@ const getUserChannelProfile = asyncHandler(async () => {
     {
       isSubscribed:{
         $cond:{
-          if:{$in:[req.user._id, "$subscibers.subscriber"]},
+          if:{$in:[req.user._id, "$subscribers.subscriber"]},
           then:true,
           else:false
         }
@@ -390,6 +390,55 @@ const getUserChannelProfile = asyncHandler(async () => {
     )
 });
 
+const getWatchHistory = asyncHandler(async(req, res)=>{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:'videos',
+        localField: "watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                 $project:{
+                  fullname:1,
+                  username:1,
+                  avatar:1
+                 }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, user[0].watchHistory, "User watch history fetched successfully")
+  )
+})
+
 export {
   registerUser,
   loginUser,
@@ -401,4 +450,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
