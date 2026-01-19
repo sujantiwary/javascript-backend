a94a27cdddb372b63f528b6e14d0ec3c2b3d9743
sujantiwary <sujantiwary5657@gmail.com>
2026-01-19 22:42:46 +0545

    refactor: reorganize imports and enhance route structure in user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserCoverImage,
+  getWatchHistory,
+  updateUserAvatar,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,11 +32,21 @@ router.post(
   registerUser
 );
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes can be added here
-router.route("/logout").post(verifyJWT, logOutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeUserPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
